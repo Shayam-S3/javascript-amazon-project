7565578869f3654d3415bbd7158074ded3cc5ef4
Shayam-S3 <sshayamsundar22@gmail.com>
2024-11-29 19:23:01 +0530

    create automates test for cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
-    cart = [{
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity: 3,
-        deliveryOptionId: '2'
-    }];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [{
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+            quantity: 3,
+            deliveryOptionId: '2'
+        }];
+    }
 }
-
 
 export function addToCart(productId) {
     let matchingItem;
