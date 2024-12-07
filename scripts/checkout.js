db43a022798293e0d597e52fe5e9d713c8a4283f
Shayam-S3 <sshayamsundar22@gmail.com>
2024-12-07 20:23:14 +0530

    convert loadProducts from XHR http request to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,11 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 
 // import '../data/backend-practice.js';
 
-new Promise((resolve)=>{
-    loadProducts(()=>{
-        resolve();
-    })
-}).then(()=>{
+loadProductsFetch().then(() => {
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
